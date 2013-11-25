5b37e8b639bf29dbdff1d6220d8442e118188e1a
Jon Rowe <hello@jonrowe.co.uk>
2013-11-25 11:54:05 +1100

    update spec syntax

--- spec/mongo/configure_spec.rb
+++ spec/mongo/configure_spec.rb
@@ -2,36 +2,33 @@ require 'mongo/configure'
 
 describe 'configuring databases' do
   describe 'from a uri' do
-    subject do
-      Mongo::Configure.from_uri('secretsquirrel://server.name:666/data').uri
-    end
-    its(:scheme)   { should == 'secretsquirrel' }
-    its(:host)     { should == 'server.name' }
-    its(:port)     { should == '666' }
-    its(:database) { should == 'data' }
-    its(:auth)     { should == '' }
-    its(:to_s)     { should == 'secretsquirrel://server.name:666/data' }
+    let(:config) { Mongo::Configure.from_uri('secretsquirrel://server.name:666/data').uri }
+
+    example { expect(config.scheme).to   eq 'secretsquirrel' }
+    example { expect(config.host).to     eq 'server.name' }
+    example { expect(config.port).to     eq '666' }
+    example { expect(config.database).to eq 'data' }
+    example { expect(config.auth).to     eq '' }
+    example { expect(config.to_s).to     eq 'secretsquirrel://server.name:666/data' }
 
     context "with authentication" do
-      subject do
-        Mongo::Configure.from_uri('secretsquirrel://squirrel:s3krit@server.name:666/data').uri
-      end
-      its(:user)     { should == 'squirrel' }
-      its(:password) { should == 's3krit' }
-      its(:auth)     { should == 'squirrel:s3krit@' }
-      its(:to_s)     { should == 'secretsquirrel://squirrel:s3krit@server.name:666/data' }
+      let(:config) { Mongo::Configure.from_uri('secretsquirrel://squirrel:s3krit@server.name:666/data').uri }
+
+      example { expect(config.user).to     eq 'squirrel' }
+      example { expect(config.password).to eq 's3krit' }
+      example { expect(config.auth).to     eq 'squirrel:s3krit@' }
+      example { expect(config.to_s).to     eq 'secretsquirrel://squirrel:s3krit@server.name:666/data' }
     end
   end
 
   describe "configuration via database name" do
-    subject do
-      Mongo::Configure.from_database("test").uri
-    end
-    its(:scheme)   { should == 'mongodb' }
-    its(:host)     { should == 'localhost' }
-    its(:port)     { should == '27017' }
-    its(:database) { should == 'test' }
-    its(:to_s)     { should == 'mongodb://localhost:27017/test' }
+    let(:config) { Mongo::Configure.from_database("test").uri }
+
+    example { expect(config.scheme).to   eq 'mongodb' }
+    example { expect(config.host).to     eq 'localhost' }
+    example { expect(config.port).to     eq '27017' }
+    example { expect(config.database).to eq 'test' }
+    example { expect(config.to_s).to     eq 'mongodb://localhost:27017/test' }
   end
 
   describe 'accessing an existing configuration' do
@@ -57,16 +54,18 @@ describe 'configuring databases' do
       configuration.connection = fake_connector
     end
 
-    subject { configuration.load }
-
     it "loads the database from uri" do
-      fake_connector.should_receive(:from_uri).with 'secretsquirrel://server.name:666/data'
-      subject
+      expect(fake_connector).to receive(:from_uri).with 'secretsquirrel://server.name:666/data'
+      configuration.load
     end
+
     it "gets the database named" do
-      fake_connection.should_receive(:db).with "data"
-      subject
+      expect(fake_connection).to receive(:db).with "data"
+      configuration.load
     end
-    it { should == database }
+
+    it 'returns the database' do
+      expect(configuration.load).to eq database
+    end
   end
 end
