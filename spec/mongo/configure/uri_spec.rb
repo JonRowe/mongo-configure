ac2612fbba9badba7c0f8ce3b2b7bda66eb4b2c2
Jon Rowe <hello@jonrowe.co.uk>
2014-07-05 10:56:31 +1000

    replace its with docstrings

--- spec/mongo/configure/uri_spec.rb
+++ spec/mongo/configure/uri_spec.rb
@@ -2,35 +2,81 @@ require 'mongo/configure/uri'
 
 describe 'parsing Mongo URIs' do
   describe 'falling back to defaults' do
-    subject { Mongo::Configure::URI.new }
-    its(:scheme)   { should == 'mongodb' }
-    its(:host)     { should == 'localhost' }
-    its(:port)     { should == '27017' }
-    its(:database) { should == '' }
-    its(:user)     { should == nil }
-    its(:password) { should == nil }
-    its(:to_s)     { should == 'mongodb://localhost:27017/' }
+    let(:uri) { Mongo::Configure::URI.new }
+    it "sets scheme" do
+      expect(uri.scheme).to eq 'mongodb'
+    end
+    it "sets host" do
+      expect(uri.host).to eq 'localhost'
+    end
+    it "sets port" do
+      expect(uri.port).to eq '27017'
+    end
+    it "sets database" do
+      expect(uri.database).to eq ''
+    end
+    it "sets user" do
+      expect(uri.user).to eq nil
+    end
+    it "sets password" do
+      expect(uri.password).to eq nil
+    end
+    it "sets to_s" do
+      expect(uri.to_s).to eq 'mongodb://localhost:27017/'
+    end
   end
   describe 'setting elements' do
-    subject { Mongo::Configure::URI.new "secretsquirrel",'server.name','666','data','user','s3kr1t' }
-    its(:scheme)   { should == 'secretsquirrel' }
-    its(:host)     { should == 'server.name' }
-    its(:port)     { should == '666' }
-    its(:database) { should == 'data' }
-    its(:user)     { should == 'user' }
-    its(:password) { should == 's3kr1t' }
-    its(:auth)     { should == 'user:s3kr1t@' }
-    its(:to_s)     { should == 'secretsquirrel://user:s3kr1t@server.name:666/data' }
+    let(:uri) { Mongo::Configure::URI.new "secretsquirrel",'server.name','666','data','user','s3kr1t' }
+    it "sets scheme" do
+      expect(uri.scheme).to eq 'secretsquirrel'
+    end
+    it "sets host" do
+      expect(uri.host).to eq 'server.name'
+    end
+    it "sets port" do
+      expect(uri.port).to eq '666'
+    end
+    it "sets database" do
+      expect(uri.database).to eq 'data'
+    end
+    it "sets user" do
+      expect(uri.user).to eq 'user'
+    end
+    it "sets password" do
+      expect(uri.password).to eq 's3kr1t'
+    end
+    it "sets auth" do
+      expect(uri.auth).to eq 'user:s3kr1t@'
+    end
+    it "sets to_s" do
+      expect(uri.to_s).to eq 'secretsquirrel://user:s3kr1t@server.name:666/data'
+    end
   end
   describe 'parsing a supplied uri' do
-    subject { Mongo::Configure::URI.parse "secretsquirrel://user:s3kr1t@server.name:666/data" }
-    its(:scheme)   { should == 'secretsquirrel' }
-    its(:host)     { should == 'server.name' }
-    its(:port)     { should == '666' }
-    its(:database) { should == 'data' }
-    its(:user)     { should == 'user' }
-    its(:password) { should == 's3kr1t' }
-    its(:auth)     { should == 'user:s3kr1t@' }
-    its(:to_s)     { should == 'secretsquirrel://user:s3kr1t@server.name:666/data' }
+    let(:uri) { Mongo::Configure::URI.parse "secretsquirrel://user:s3kr1t@server.name:666/data" }
+    it "sets scheme" do
+      expect(uri.scheme).to eq 'secretsquirrel'
+    end
+    it "sets host" do
+      expect(uri.host).to eq 'server.name'
+    end
+    it "sets port" do
+      expect(uri.port).to eq '666'
+    end
+    it "sets database" do
+      expect(uri.database).to eq 'data'
+    end
+    it "sets user" do
+      expect(uri.user).to eq 'user'
+    end
+    it "sets password" do
+      expect(uri.password).to eq 's3kr1t'
+    end
+    it "sets auth" do
+      expect(uri.auth).to eq 'user:s3kr1t@'
+    end
+    it "sets to_s" do
+      expect(uri.to_s).to eq 'secretsquirrel://user:s3kr1t@server.name:666/data'
+    end
   end
 end
